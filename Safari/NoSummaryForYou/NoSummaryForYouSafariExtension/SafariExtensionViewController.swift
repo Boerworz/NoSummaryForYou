ad24b79a36bf428af3b99aeb5fbda7c0998c6627
Tim Andersson <tim@ndersson.me>
2017-07-08 22:50:33 +0200

    First attempt at a Safari Extension for Netflix
    The extension hides all summaries (that I’ve found) on Netflix and allows
    the user to view the summaries by placing their cursor on top of them.

---
+++ Safari/NoSummaryForYou/NoSummaryForYouSafariExtension/SafariExtensionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SafariExtensionViewController.swift
+//  NoSummaryForYouSafariExtension
+//
+//  Created by Tim Andersson on 2017-06-11.
+//  Copyright © 2017 Cocoabeans Software. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
