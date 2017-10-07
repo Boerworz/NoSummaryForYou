670669de62085129de7ad5916b80b1bd57ffcf01
Tim Andersson <tim@ndersson.me>
2017-10-07 18:08:44 +0200

    [Firefox] Allow the user to toggle blur by clicking toolbar item

---
+++ Firefox/Scripts/BackgroundScripts/netflix.js
@@ -0,0 +1,32 @@
+/*
+ * BackgroundScripts/netflix.js is a so called Background Script
+ * (https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Anatomy_of_a_WebExtension#Background_scripts)
+ * and is responsible for managing the page action (i.e. toolbar item). The 
+ * responsibilities include making sure the page action is shown (i.e. enabled)
+ * on the Netflix webpage and handling the event that is generated when the page
+ * action is clicked.
+ *
+ * The JavaScript that actually does something useful when the page action is
+ * clicked lives in ContentScripts/netflix.js.
+ */
+
+browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	var isNetflixTab = tab.url.indexOf("netflix.com") != -1;
+	if (isNetflixTab) {
+		browser.pageAction.show(tab.id);
+	} else {
+		browser.pageAction.hide(tab.id);
+	}
+});
+
+browser.pageAction.onClicked.addListener(function(tab) {
+	browser.tabs.sendMessage(tab.id, {"name": "toolbarItemClicked"}, function(response) {
+		// TODO: Change the icon instead of the tooltip title
+		if (response.isBlurEnabled) {
+			browser.pageAction.setTitle({"title": "Enabled", "tabId": tab.id});
+		} else {
+			browser.pageAction.setTitle({"title": "Disabled", "tabId": tab.id});
+		}
+	});
+});
+
