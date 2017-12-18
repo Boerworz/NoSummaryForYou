eec8a0cc38b5417833aa5344a7895312119e89f1
Tim Andersson <tim@ndersson.me>
2017-12-18 19:54:59 +0100

    [Safari] Disable toolbar item for unsupported sites

--- Safari/NoSummaryForYou/NoSummaryForYouSafariExtension/SafariExtensionHandler.swift
+++ Safari/NoSummaryForYou/NoSummaryForYouSafariExtension/SafariExtensionHandler.swift
@@ -43,9 +43,17 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     }
     
     override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
-        // This is called when Safari's state changed in some way that would require the extension's toolbar item to be validated again.
-        validationHandler(true, "")
-    }
+		window.getActiveTab { (tab) in
+			tab?.getActivePage { (page) in
+				page?.getPropertiesWithCompletionHandler { (pageProperties) in
+					// We can't get the url for a tab that is visiting a domain that we haven't declared
+					// in our Info.plist, so if we get `nil` back we know we should disable the toolbar item.
+					let isVisitingSupportedDomain = pageProperties?.url != nil
+					validationHandler(isVisitingSupportedDomain, "")
+				}
+			}
+		}
+	}
     
     override func popoverViewController() -> SFSafariExtensionViewController {
         return SafariExtensionViewController.shared
