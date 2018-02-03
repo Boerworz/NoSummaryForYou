23da6159a9e7b71c7b680cda9fa0acc965fba890
Tim Andersson <tim@ndersson.me>
2018-02-03 16:11:00 +0100

    Allow process_templates.sh to be run from anywhere
    Prior to this commit the script only worked as expected if run from the
    project root directory using ./tools/process_templates.sh.

--- tools/process_templates.sh
+++ tools/process_templates.sh
@@ -8,6 +8,7 @@
 # Invoke it without any arguments (e.g. ./tools/process_templates.sh) whenever
 # you've made changes to the selectors list or the template files.
 
-project_root_directory=$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" ; pwd -P)
+script_directory=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+project_root_directory=$(cd "$(dirname "$script_directory")" ; pwd -P)
 
 find "$project_root_directory" -name "*.template" -exec "$project_root_directory/tools/process_templates.py" {} +
